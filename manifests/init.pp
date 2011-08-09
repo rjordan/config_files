214fe2f79a920cca57bee9e88c7533ac04846c90
Richard Kent Jordan <rjordan@pobox.com>
2011-08-08 19:28:09 -0500

    First commit

---
+++ manifests/init.pp
@@ -0,0 +1,20 @@
+class core {
+  package {
+    "git": ensure=>latest;
+    "etckeeper": ensure=>latest;
+    "ssh": ensure=>latest;
+    "denyhosts": ensure=>latest;
+    "ntp": ensure=>latest;
+    "htop": ensure=>latest;
+    "sudo": ensure=>latest;
+  }
+  service {
+    "ssh": require=>Package["ssh"], ensure=>running;
+    "ntp": require=>Package["ntp"], ensure=>running;
+  }
+  file {
+    "/etc/sudoers": owner=>root,group=>root,mode=>"0440";
+    "/etc/etckeeper/etckeeper.conf": owner=>root,group=>root,
+      mode=>"0644",require=>Package["sudo"]
+  }
+}
